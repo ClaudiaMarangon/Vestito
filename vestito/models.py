2084d19bfc6dcaf1d809861be2b6180ea0201b06
ClaudiaMarangon <claudia.marangon@studbocconi.it>
2017-12-11 22:35:23 +0100

    recommit

--- vestito/models.py
+++ vestito/models.py
@@ -285,14 +285,10 @@ class Player(BasePlayer):
 
             return configs
 
-    def random_round(self):
-        if self.round_number == 3:
-            rn = random.randint(1, 3)
-            return rn
-
     def final_payoff(self):
         if self.round_number==3:
-            finalpay = self.in_round(self.random_round()).payoff
+            rn = random.randint(1, 3)
+            finalpay = self.in_round(rn).payoff
             return finalpay
 
     pass
